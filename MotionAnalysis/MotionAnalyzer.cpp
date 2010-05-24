e7736bd7f7a6116c71896cc77110c50283a5dbd2
stonedgorilla <stonedgorilla@c577ff7b-230a-bcd6-8990-922772cbb5c1>
2010-05-24 20:43:28 +0000

    AverageVelocity computation

--- MotionAnalysis/MotionAnalyzer.cpp
+++ MotionAnalysis/MotionAnalyzer.cpp
@@ -34,20 +34,42 @@ double MotionAnalyzer::getTimeFromFrame( const int& nFrame ){
 }
 
 void MotionAnalyzer::computeVelocities(void){
-  //Call a curvefitting
+  if( points.size() < 2 ) return;
+
   std::vector<cv::Point2f>::const_iterator p;
   std::vector<double>::const_iterator t;
-  double dt(0.0);
-  cv::Point2f dp;
-  for( p = points.begin(), t = time.begin();
-       (p + 1) != points.end(); ++p, ++t){
-    dt = (*t) - *(t+1);
-  }  
+
+  p = points.end() - 1;
+  t = time.end() - 1;
+
+  double dt      = *t - *(t - 1);
+  cv::Point2f dp = *p - *(p - 1);
+
+  dp.x /= static_cast<float>(dt);
+  dp.y /= static_cast<float>(dt);
+
+  veloc.push_back( dp );
+
+  if( veloc.size() == 1 ){
+    velocity = veloc.back();
+    return;
+  }
+
+  //Compute the average on-line
+  //Reusing dp for computing the average velocity
+  velocity.x *= static_cast<float>(veloc.size() - 1);
+  velocity.y *= static_cast<float>(veloc.size() - 1);
+
+  velocity  += veloc.back();
+
+  velocity.x /= static_cast<float>(veloc.size());
+  velocity.y /= static_cast<float>(veloc.size());
 }
 
 void MotionAnalyzer::drawTrajectory(cv::Mat& frame){
   if( points.size() < 2 ) return;
-
+ 
+  //Draw lines per frame
   for( std::vector<cv::Point2f>::const_iterator p = points.begin();
        (p + 1) != points.end(); ++p){
     cv::line( frame, *p, *(p+1), cv::Scalar(255.0, 0.0, 0.0) );
