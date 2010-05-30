46fb554b6ee417b7b3d1dc23bc0948dc23a0841c
Saiphcita <Saiphcita@c577ff7b-230a-bcd6-8990-922772cbb5c1>
2010-05-30 04:46:39 +0000

    version a prueba

--- trunk/test/HocTest.cpp
+++ trunk/test/HocTest.cpp
@@ -12,58 +12,12 @@ using namespace hog;
 
 int main(int argc, char** argv)
 {
-	float **trainingData;
-	//float **test=new float*[1];
-	int size;
-	int i=0;
-	
-	
-	trainingData=new float*[1007];
-   
-    prepareTrainData(924,"per00", size,trainingData,8,4,i);
-     prepareTrainData(1007,"bikeList.lst", size,trainingData,8,4,i,"/home/saiph/281b/final-ece-cs281b/test/bike/");
-   
-   /*
-   //prepareTrainData(831,"pos.txt", size,trainingData,8,4,i,"/home/saiph/281b/final-ece-cs281b/test/");
-   //prepareTrainData(924,"per00", size,trainingData,8,4,i);
-   //prepareTrainData(999,"trainPos.lst", size,trainingData,8,4,i,"/home/saiph/281b/final-ece-cs281b/test/");
-  // prepareTrainData(924,"per00", size,trainingData,8,4,i);
-   //prepareTrainData(1639,"trainNeg.lst", size,trainingData,8,4,i,"/home/saiph/281b/final-ece-cs281b/test/");*/
-   cout<<"valor i"<<i;
-  cv::Mat tset(1007, size, CV_32FC1,trainingData);
-   float tLables[1007];
-	
-	for(int i=0;i<924;i++)
-	{
-		tLables[i]=1.0;
-	}
-	for(int i=924;i<1007;i++)
-	{
-		tLables[i]=-1.0;
-	}
-	cv::Mat labels(1007, 1, CV_32FC1,tLables);
-	
-	CvSVM svm;
-	
-  	svm.train(tset, labels);
-  	
-  	ofstream myfile;
-  	myfile.open ("output.txt");
-  	for( int i = 0; i <1007; i++){
-    	float cls = svm.predict(tset.row(i));
-    	cout<<"i value: "<<i;
-    	cout << endl<<"Class: " <<fixed << cls <<endl;
-    	myfile <<"i value: "<<i;
-    	myfile<<endl<<"Class: " <<fixed << cls <<endl;
-  	}
-  	
-  	
-  	myfile.close();
-  	svm.save("svmPersons.xml");
-    
-	
-	
-  	//svm.save("svmPersons.xml");*/
-    
+	Mat img = imread("/home/saiph/281b/final-ece-cs281b/test/bike/train/pos/person_and_bike_143c.png");
+    if( !img.data ) {
+          cerr << "Cannot open File" << endl;
+          return 1;
+        }
+        
+    predictionHOG(img);
 	return 0;
 }
