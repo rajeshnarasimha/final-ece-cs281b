3f7ee66d4d9e7b5e0e92555c6f9921535d654542
Saiphcita <Saiphcita@c577ff7b-230a-bcd6-8990-922772cbb5c1>
2010-06-01 04:57:54 +0000

    con los paramtetros d bines por default

--- trunk/include/descriptors/hog.h
+++ trunk/include/descriptors/hog.h
@@ -27,20 +27,20 @@ void write(float **trainingData,int size, int numTrain,std::string nameFile);
  
  double calculateL2Norm(cv::MatND matricita);
  
- void obtainTestDescriptor(std::string fileName, std::string outputName,int & size,float **descriptor,int sizeCeldas,int sizeBlock);
+ //void obtainTestDescriptor(std::string fileName, std::string outputName,int & size,float **descriptor,int sizeCeldas,int sizeBlock);
  
- float ** trainEx(std::string fileName,int trainEx,float **trainingData,std::string nameFileWrite,int & size,int windowSize,int blockSize);
+ float ** trainEx(std::string fileName,int trainEx,float **trainingData,std::string nameFileWrite,int & size,int windowSize,int blockSize, int binSize);
   
  void prepareDerImages(std::string pictureName,cv::Mat & dx, cv::Mat & dy);
- float ** trainEx(cv::Mat originalImage,int trainEx,float **trainingData,std::string nameFileWrite,int & sizeInput,int windowSize,int blockSize);
+ float ** trainEx(cv::Mat originalImage,int trainEx,float **trainingData,std::string nameFileWrite,int & sizeInput,int windowSize,int blockSize, int binSize);
  void prepareDerImages(cv::Mat img,cv::Mat & dx, cv::Mat & dy); 
  
  std::vector <cv::Mat> normalizeBlock(std::vector <cv::MatND>   block, double e);
- float predictionHOG(cv::Mat,int numBlocks,int numCeldas);
+ float predictionHOG(cv::Mat,int numBlocks,int numCeldas,int binSize);
  float ** integrate( std::vector<cv::Mat>block, float  ** a, int & indice,int trainEx,int size,int windowSize);
- float ** prepareTrainData(int numTrainExample,std::string fileName, int & size,float **trainingData,int sizeCeldas,int sizeBlock,int & i,std::string ruta );
+ float ** prepareTrainData(int numTrainExample,std::string fileName, int & size,float **trainingData,int sizeCeldas,int sizeBlock,int & i,std::string ruta,int binSize);
  
- float ** prepareTrainData(int numTrainExample,std::string fileName, int & size,float **trainingData,int sizeCeldas,int sizeBlock,int & i);
+ float ** prepareTrainData(int numTrainExample,std::string fileName, int & size,float **trainingData,int sizeCeldas,int sizeBlock,int & i, int binSize);
 
 void computeBlocks(int & sizeInput,double e,std::vector< std::vector<cv::MatND> > rowsOfCells,int windowSize,const int blockSize,std::vector< std::vector <cv::Mat> > & normalizedBlocks);
  
